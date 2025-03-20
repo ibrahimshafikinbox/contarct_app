863777647249128f76bf22578f60e6a35290f97b
Ibrahim Shafik <110847489+ibrahimshafikinbox@users.noreply.github.com>
2025-03-20 04:24:01 +0200

    Update constants.dart

--- lib/Core/constants/constants.dart
+++ lib/Core/constants/constants.dart
@@ -1,16 +1,9 @@
 class AppConstants {
-  // static const String client_id =
-  //     'Aac3nyFasvRGIqePykCsaLoUeS5vb0PJOvCkgq70c91UgxACs4ctQ1oOdECgTBroK99corOueFB0wFQE';
-  // static const String secret_key =
-  //     'ED6W1-TqP9MnpGanWaJ9jHFbe34-goppUKKgRC09hRh9DgWJ6E_z7B9zcDsKQ6bK7jkt477LferePrby';
-  // static const String google_pay_api_key =
-  //     "AIzaSyCpDaL_glHwiD7K9thhOERYriAUMW1h0rA";
-  // static const String google_pay_merchant_id = "5538129902";
-
+  
   static const String stipe_publishable =
-      "pk_test_51QwpA4Q56YoeF1W8dZAkunFssAJA1j7kB07JHHLNXREEdi0CuZdPNmHjwcOBSiW3toXshRNcdG3nAojmVfyLRshx00gU4IA8Hh";
+      " ";
   static const String strip_secret =
-      "sk_test_51QwpA4Q56YoeF1W8WQGidzyZWXDpiNE1lA6kIaRRs3g26q0e2vbV3g0yTNKmvE35wy2Ds1ttii6rGk0ApwXLtfln00RZl4V7gN";
+      " ";
   static const String stipe_public =
-      "pk_test_51Qp3zqLFpaB8SPtuFsBviTLoEPIb1xboTiP7y8g5CcvqnXAo6sDdcXjJaSxSimg7bM8zaSg9cgX70SJ8MWwlueWM00N9mJCzFF";
+      " ";
 }
